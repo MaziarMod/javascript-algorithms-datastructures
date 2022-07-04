6d950e849a307dfec6985053b9c417976d01d7a1
Maziar <Maziar.mod@gmail.com>
2022-07-04 18:58:00 -0400

    add anagramBigORefactored.js

---
+++ 007-anagram-BigO/angramBigORefactored.js
@@ -0,0 +1,36 @@
+function validAnagram(first, second) {
+
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] = lookup[letter] + 1 || 1;
+    }
+    console.log(lookup)
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
+}
+
+
+t1 = performance.now();
+
+// {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+console.log(validAnagram('anagrams', 'nagaramm'));
+
+t2 = performance.now();
+console.log(`time of his function is : ${(t2 - t1)/1000} seconds.`);
