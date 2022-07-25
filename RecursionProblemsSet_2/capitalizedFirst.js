dc6a15bd400ac5138aec64560eea0daf742a7d68
Maziar <Maziar.mod@gmail.com>
2022-07-24 20:14:36 -0400

    add recurive problems

---
+++ RecursionProblemsSet_2/capitalizedFirst.js
@@ -0,0 +1,12 @@
+/*
+Write a recursive function called capitalizeFirst.
+Given an array of strings, capitalize the first letter of each string in the array.
+
+capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+ */
+
+function capitalizeFirst () {
+    // add whatever parameters you deem necessary - good luck!
+}
+
+console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
