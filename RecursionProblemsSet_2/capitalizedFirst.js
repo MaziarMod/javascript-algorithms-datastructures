ec3b016714e84391e04e9ec1b6359a76e284b1f8
Maziar <Maziar.mod@gmail.com>
2022-07-27 08:41:13 -0400

    add recurive problems set 2 part 1

--- RecursionProblemsSet_2/capitalizedFirst.js
+++ RecursionProblemsSet_2/capitalizedFirst.js
@@ -5,8 +5,17 @@ Given an array of strings, capitalize the first letter of each string in the arr
 capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
  */
 
-function capitalizeFirst () {
-    // add whatever parameters you deem necessary - good luck!
+function capitalizeFirst(strArry) {
+     if (strArry.length === 1) {
+          return [strArry[0][0].toUpperCase() + strArry[0].slice(1)];
+     }
+     
+     const capitalized = capitalizeFirst(strArry.slice(0, -1)); //1. [car, taco] 2.[car] 3.[]
+     
+     const string = strArry.slice(-1)[0][0].toUpperCase() + strArry.slice(-1)[0].slice(1);
+     capitalized.push(string);
+     
+     return capitalized;
 }
 
 console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
